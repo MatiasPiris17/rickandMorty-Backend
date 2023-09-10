f425f126a6be94018547e7f41e6e6d98a6cba3b7
Matias <matiaslinkedin17@gmail.com>
2023-09-10 12:23:56 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const cookieParser = require("cookie-parser");
+
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const router = require("./routes/index.js");
+
+const { sequelize } = require("./database.js");
+
+const server = express();
+
+const corsOptions = {
+  origin: "*",
+};
+
+const port = process.env.PORT || 3001;
+
+server.use(cookieParser());
+
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.use(bodyParser.json());
+
+server.use(cors(corsOptions));
+
+server.use("/", router);
+
+server.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json("Internal Server Error");
+});
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("Synchronized tables");
+    server.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error synchronizing models: ${err.message}`);
+  });
